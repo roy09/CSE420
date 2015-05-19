c5bc437f5e4948020c085ff5d30ce4e221b0929f
Jyotirmoy Roy= <nextgen.ancestor@gmail.com>
2015-05-19 23:55:48 +0600

    without identifier done

--- Lab01/build1.py
+++ Lab01/build1.py
@@ -1,6 +1,67 @@
+import re
 
+# define the keywords
+KEYWORDS = {"if", "else", "for", "while", "true", "int", "double"}
+DATA_TYPE = {"int", "double"}
+MATH_OPS = {'+', '-', '*', '/', '%'}
+LOGIC_OPS = {'>', '<' '=='}
+# define the to be returned structures
+keywords = []
+identifiers = []
+mathOps = []
+logicOps = []
+numVals = []
+others = []
 
-# print hello world
 with open("data.txt") as input:
     for line in input:
-        print line
+        temp = re.split(';', line)
+        temp = temp[0].split()
+
+        # check it is a keyword
+        for item in temp:
+            if item in KEYWORDS:
+                keywords.append(item)
+                continue
+
+        # check if is a math operation
+        for item in temp:
+            if item in MATH_OPS:
+                mathOps.append(item)
+                continue
+
+
+        # check if it is a logical operation
+        for item in temp:
+            if item in LOGIC_OPS:
+                logicOps.append(item)
+                continue
+
+
+
+        # check if it is a numeric value
+        if item.isdigit():
+            numVals.append(item)
+            continue
+        try:
+            float(item)
+            numVals.append(item)
+            continue
+        except ValueError:
+            pass
+
+
+        others.append(item)
+
+print "Keywords: "
+print keywords
+print "Identifiers: "
+print identifiers
+print "Math Operations: "
+print mathOps
+print "Logic Operations: "
+print logicOps
+print "Numeric Values: "
+print numVals
+print "Others: "
+print others
